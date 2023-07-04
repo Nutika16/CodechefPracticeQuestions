dd321e29ab81b16120b96547b97fba86b372b66d
Nutika <90458419+Nutika16@users.noreply.github.com>
2023-07-04 20:57:01 +0530

    Burgers.java

--- Burgers.java
+++ Burgers.java
@@ -1,1 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int a=sc.nextInt(); //no of patties
+		    int b=sc.nextInt(); //no of buns
+		    if(a<b){
+		        System.out.println(a);
+		    }
+		    else{
+		        System.out.println(b);
+		    }
+		    
+		}
+	}
+}
 
